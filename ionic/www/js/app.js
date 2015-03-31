f8207b71f4036cc1bc18e68cee4ae7a61300605c
martinmicunda <martinmicunda@hotmail.com>
2015-04-01 00:06:28 +0100

    chore(*): add copyright

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -1,10 +1,16 @@
+/**
+ * Main app module.
+ *
+ * @author    Martin Micunda {@link http://martinmicunda.com}
+ * @copyright Copyright (c) 2015, Martin Micunda
+ * @license   The MIT License {@link http://opensource.org/licenses/MIT}
+ */
 (function () {
     'use strict';
 
     angular.module('app', [
         // angular modules
         'ngAnimate',
-        //'ngMessages',
         'ngSanitize',
 
         // 3rd party modules
