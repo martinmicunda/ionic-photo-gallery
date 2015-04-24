527407a8e67bf0ef62e5dc59c6c61d9fc95e460e
martinmicunda <martinmicunda@hotmail.com>
2015-04-24 13:38:51 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @author    Martin Micunda {@link http://martinmicunda.com}
+ * @copyright Copyright (c) 2015, Martin Micunda
+ * @license	  The MIT License {@link http://opensource.org/licenses/MIT}
+ */
+'use strict';
+
+/**
+ * Start ionic-photo-gallery application.
+ */
+require('ionic-photo-gallery-server');
+
+
+
