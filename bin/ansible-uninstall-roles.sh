28a3cc9cca31ebaee6360ac22b6add0f7c8b0427
Javier Pais <javi.pais@yahoo.es>
2018-10-07 20:27:17 +0200

    nickpack doesn't seem to be valid anymore

--- bin/ansible-uninstall-roles.sh
+++ bin/ansible-uninstall-roles.sh
@@ -8,4 +8,4 @@ ansible-galaxy remove martinmicunda.common \
                       Stouts.mongodb \
                       DavidWittman.redis \
                       williamyeh.oracle-java \
-                      nickp666.android-sdk
+                      nickpack.android_sdk
